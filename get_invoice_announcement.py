e24d3f6375239f0fada17937ff92600dff3494ae
ikuma <chemicalbondo@gmail.com>
2022-09-17 17:24:12 +0900

    docstring追加と微修正

--- get_invoice_announcement.py
+++ get_invoice_announcement.py
@@ -6,10 +6,20 @@ from dotenv import load_dotenv
 
 from get_registration_numbers import RegistrationNumbsers
 
-BASE_URL = 'https://web-api.invoice-kohyo.nta.go.jp/1/num'
-
 
 def get_invoice_announcement() -> list[dict]:
+    """
+    インボイス公表システムWeb-APIから公表情報をjson形式で返却。\n
+
+    インボイス公表システム(https://www.invoice-kohyo.nta.go.jp/)
+    に登録番号を投げ公開情報を取得、json形式で返却。\n
+    同階層の registration_numbers.csv に記載された番号を基に取得する。
+
+    :rtype: list[dict]
+    :return: インボイス公開情報をjsonで返す
+    :raise: err: apiリクエスト失敗
+    """
+
     register_numbers = RegistrationNumbsers()
     register_numbers.get_number_list()
 
@@ -22,7 +32,8 @@ def get_invoice_announcement() -> list[dict]:
     }
 
     try:
-        response: requests.Response = requests.get(BASE_URL, params=parameters)
+        base_url = 'https://web-api.invoice-kohyo.nta.go.jp/1/num'
+        response: requests.Response = requests.get(base_url, params=parameters)
         response.raise_for_status()
         corporates: list[dict] = response.json()["announcement"]
     except requests.exceptions.RequestException as err:
@@ -32,6 +43,12 @@ def get_invoice_announcement() -> list[dict]:
 
 
 def write_invoice_announcement(corporates: list[dict]):
+    """
+    json形式で渡された情報をもとに同階層に invoice.csv を作成する。\n
+    :param list[dict] corporates:
+    :return:
+    """
+
     invoice_rows = [
         [
             corporate["registratedNumber"],
